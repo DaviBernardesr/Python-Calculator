8b01800a41bfe29eaad82e983de63f7e5e2bbbc4
DaviBernardesr <davibernardesmoto@gmail.com>
2026-01-08 22:22:06 -0300

    operações básicas

--- main.py
+++ main.py
@@ -3,6 +3,16 @@
 from operacoes import operacoes
 import math
 
+#Global
+result = 0
+
+from operacoes.operacoes import adicao
+from operacoes.operacoes import subtracao
+from operacoes.operacoes import multiply
+from operacoes.operacoes import poten
+from operacoes.operacoes import divis
+
+
 #Inicializando.
 num = 0
 
@@ -18,37 +28,39 @@ while (num == 0):
     print("-")
     print("*")
     print("** (Potência)")
-    print("/ (Divisão\n")
+    print("/ (Divisão)\n")
 
-    print("x = Sair")
+    print(f"Visor: {result}")
+
+    print("x = Sair\n")
 
     operacao = input("Digite qual operação realizar:\n")
 
     if (operacao == "+"):
         num1 = float(input("Digite um número:"))
         num2 = float(input("Digite outro número:"))
-        print(num1)
-        print(num2)
+        result = adicao(num1, num2)
+        print(f"{result}")
     elif (operacao == "-"):
         num1 = float(input("Digite um número:"))
         num2 = float(input("Digite outro número:"))
-        print(num1)
-        print(num2)
+        result = subtracao(num1, num2)
+        print(f"{result}")
     elif (operacao == "*"):
         num1 = float(input("Digite um número:"))
         num2 = float(input("Digite outro número:"))
-        print(num1)
-        print(num2)
+        result = multiply(num1, num2)
+        print(f"{result}")
     elif (operacao == "**"):
-        num1 = float(input("Digite um número:"))
-        num2 = float(input("Digite outro número:"))
-        print(num1)
-        print(num2)
+        num1 = float(input("Digite a base:"))
+        num2 = float(input("Digite a potência:"))
+        result = poten(num1, num2)
+        print(f"{result}")
     elif (operacao == "/"):
         num1 = float(input("Digite um número:"))
         num2 = float(input("Digite outro número"))
-        print(num1)
-        print(num2)
+        result = divis(num1, num2)
+        print(f"{result}")
     elif (operacao == "x"):
         print("Saindo da Calculadora...\n")
         exit()
--- operacoes/operacoes.py
+++ operacoes/operacoes.py
@@ -2,3 +2,22 @@
 
 #módulos
 
+def adicao(num1, num2):
+    result = num1 + num2
+    return result
+
+def subtracao(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def poten(num1, num2):
+    result = num1 ** num2
+    return result
+
+def divis(num1, num2):
+    result = num1 / num2
+    return result
